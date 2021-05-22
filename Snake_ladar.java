f97d66199513f63cc60ac6011bc1c45056f34fbe
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-22 10:29:41 +0530

    [Ranjith] added Snake_ladar.java

--- Snake_ladar.java
+++ Snake_ladar.java
@@ -1,1 +1,13 @@
 import java.util.*;
+class Demo{
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+	static int player1_Position= 0;
+	static int player2_Position= 0;
+}
+public class Snake_ladar {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}
